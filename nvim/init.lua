106d3dce627bbb152a84d43b2c2451465c7206c6
Aming Lau <cyberming@gmail.com>
2023-03-05 15:14:31 -0800

    Move options to options.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,20 +14,6 @@ vim.g.mapleader = ','           -- Change the leader key from '/' to ','
 require'plugins'
 require'styles'
 require'lsp'
-
--- Options
-vim.o.showcmd = true            -- Display incomplete commands
-vim.o.wildmenu = true           -- Enable command-line completion operation
-vim.o.ignorecase = true         -- No case sensitive search
-vim.o.smartcase = true          -- Ignore 'ignorecase' if the search pattern contains upper case
-vim.o.expandtab = true          -- Replace tab with space
-vim.o.smarttab = true           -- <BS> delete a shiftwidth
-vim.o.smartindent = true        -- Autoindenting when starting a new line
-vim.o.mouse = 'a'               -- Enable mouse operation
-vim.o.number = true             -- Show line numbers
-vim.o.colorcolumn = '120'       -- Set the line on the 120 coloumn
-vim.o.tabstop = 2               -- Set the line on the 120 coloumn
-vim.o.shiftwidth = 2            -- Set the line on the 120 coloumn
-
+require'user.options'
 
 -- vim: ts=2 sw=2 ft=lua expandtab:
---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,43 @@
+-- :help options
+local options = {
+  backup = false,                         -- Don't creates a backup file
+  swapfile = false,                       -- creates a swapfile
+  fileencoding = "utf-8",                 -- the encoding written to a file
+  mouse = 'a',                            -- Enable mouse operation
+
+  cmdheight = 2,                          -- more space in the neovim command line for displaying messages
+  showcmd = true,                         -- Display incomplete commands
+  wildmenu = true,                        -- Enable command-line completion operation
+  pumheight = 10,                         -- pop up menu height
+  signcolumn = "yes",                     -- Always shows Sign column to avoid shifting (Default: Auto)
+  number = true,                          -- Show line numbers
+  relativenumber = false,                 -- Set relative numbered lines
+  numberwidth = 2,                        -- set number column width to 2 {default 4}
+  colorcolumn = '120',                    -- Set a vertical line on the 120 coloumn
+  wrap = false,                           -- display lines as one long line
+  scrolloff = 2,                          -- Minimum number of lines be kept above and below the cursor
+  sidescrolloff = 4,                      -- Minimum number of column be kept to the right
+  cursorline = true,                      -- highlight the current line
+
+  splitbelow = true,                      -- force all horizontal splits to go below current window
+  splitright = true,                      -- force all vertical splits to go to the right of current window
+
+  ignorecase = true,                      -- Case-insensitive search
+  smartcase = true,                       -- Ignore 'ignorecase' if the search pattern contains upper case
+  hlsearch = true,                        -- highlight all matches on previous search pattern
+
+  expandtab = true,                       -- Replace tabs to spaces
+  smarttab = true,                        -- <BS> delete a shiftwidth
+  showtabline = 2,                        -- always show tabs
+  tabstop = 2,                            -- Set the tab stop
+  shiftwidth = 2,                         -- the number of spaces inserted for each indentation
+  smartindent = true,                     -- Autoindenting when starting a new line
+
+  completeopt = { "menuone", "noselect" },-- mostly just for cmp
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+-- vim: ts=2 sw=2 ft=lua expandtab:
