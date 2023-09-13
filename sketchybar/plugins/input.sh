b83c4c99a893fa8bf27e931a4c9cff7c7d6db3e1
Aming Lau <cyberming@gmail.com>
2023-09-13 16:34:36 -0700

    Set the default input source as ?

--- sketchybar/plugins/input.sh
+++ sketchybar/plugins/input.sh
@@ -2,13 +2,12 @@
 
 SOURCE=$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleCurrentKeyboardLayoutInputSourceID)
 
-LABEL='?'
-
 case ${SOURCE} in
   'com.apple.keylayout.ABC') LABEL='A' ;;
   'com.apple.keylayout.Dvorak') LABEL='Dv' ;;
   'com.apple.keylayout.CangjieKeyboard') LABEL='倉' ;;
   'com.apple.keylayout.TraditionalPinyinKeyboard') LABEL='拼' ;;
+  *) LABEL='?' ;;
 esac
 
 sketchybar --set $NAME label="$LABEL"
